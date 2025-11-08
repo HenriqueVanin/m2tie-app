2eb91db28790b935e5100bfb319975790637c090
HenriqVanin <henrique@datadriva.com>
2025-11-08 18:06:55 -0300

    base repo update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/m2tie-app",
+  base: "/m2tie-app/",
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
